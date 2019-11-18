abc34e1046bbf28449711b9ba560b33f49cc542f
Dror <dror@rndrobin.com>
2019-11-18 15:25:14 +0200

    Try fix normal build for glew

--- libavfilter/vf_genericshader.c
+++ libavfilter/vf_genericshader.c
@@ -4,7 +4,10 @@
 #ifdef __APPLE__
 #include <OpenGL/gl3.h>
 #else
-#include <GL/glew.h>
+    #ifndef GLEW_STATIC
+        #define GLEW_STATIC
+    #endif
+    #include <GL/glew.h>
 #endif
 
 #include <GLFW/glfw3.h>
