347f970acc0cbd33f4d3a6bba948bd59dca42f74
Dror <dror@rndrobin.com>
2019-11-21 14:45:42 +0200

    Add more shaders

--- libavfilter/vf_genericshader.c
+++ libavfilter/vf_genericshader.c
@@ -46,6 +46,7 @@ typedef struct GenericShaderContext {
     float power;
 
     int eval_mode;              ///< EvalMode
+    int is_color; // for vintage filter
 
     double var_values[VAR_VARS_NB];
     char *power_expr;
@@ -69,6 +70,12 @@ enum EvalMode {
     EVAL_MODE_NB
 };
 
+enum IsColorMode {
+    IS_COLOR_MODE_TRUE,
+    IS_COLOR_MODE_FALSE,
+    IS_COLOR_MODE_NB
+}
+
 static const float position[12] = {
   -1.0f, -1.0f,  //A
   1.0f, -1.0f,  // B
@@ -82,8 +89,8 @@ static const GLchar *v_shader_source =
   "attribute vec2 position;\n"
   "varying vec2 texCoord;\n"
   "void main(void) {\n"
-  "  gl_Position = vec4(position* 0.5 + 0.5, 0, 1);\n"
-  "  texCoord = position;\n"
+  "  gl_Position = vec4(position, 0, 1);\n"
+  "  texCoord = position* 0.5 + 0.5;\n"
   "}\n";
 
 static const GLchar *f_shader_source =
@@ -92,6 +99,128 @@ static const GLchar *f_shader_source =
   "void main() {\n"
   "  gl_FragColor = texture2D(tex, texCoord);\n"
   "}\n";
+
+// Thanks to luluco250 - https://www.shadertoy.com/view/4t2fRz
+static const GLchar *f_vintage_shader_source =
+"// 0: Addition, 1: Screen, 2: Overlay, 3: Soft Light, 4: Lighten-Only\n"
+"#define BLEND_MODE 0\n"
+"#define SPEED 2.0\n"
+"#define INTENSITY 0.075\n"
+"// What gray level noise should tend to.\n"
+"#define MEAN 0.0\n"
+"// Controls the contrast/variance of noise.\n"
+"#define VARIANCE 0.5\n"
+
+"vec3 channel_mix(vec3 a, vec3 b, vec3 w) {\n"
+"    return vec3(mix(a.r, b.r, w.r), mix(a.g, b.g, w.g), mix(a.b, b.b, w.b));\n"
+"}\n"
+
+"float gaussian(float z, float u, float o) {\n"
+"    return (1.0 / (o * sqrt(2.0 * 3.1415))) * exp(-(((z - u) * (z - u)) / (2.0 * (o * o))));\n"
+"}\n"
+
+"vec3 madd(vec3 a, vec3 b, float w) {\n"
+"    return a + a * b * w;\n"
+"}\n"
+
+"vec3 screen(vec3 a, vec3 b, float w) {\n"
+"    return mix(a, vec3(1.0) - (vec3(1.0) - a) * (vec3(1.0) - b), w);\n"
+"}\n"
+
+"vec3 overlay(vec3 a, vec3 b, float w) {\n"
+"    return mix(a, channel_mix(\n"
+"        2.0 * a * b,\n"
+"        vec3(1.0) - 2.0 * (vec3(1.0) - a) * (vec3(1.0) - b),\n"
+"        step(vec3(0.5), a)\n"
+"    ), w);\n"
+"}\n"
+
+"vec3 soft_light(vec3 a, vec3 b, float w) {\n"
+"    return mix(a, pow(a, pow(vec3(2.0), 2.0 * (vec3(0.5) - b))), w);\n"
+"}\n"
+"varying vec2 texCoord;\n"
+"uniform sampler2D text;\n"
+"uniform float power;\n"
+"uniform float time;\n"
+"uniform bool isColor;\n"
+
+"void main() {\n"
+"    vec2 uv = texCoord;\n"
+"    vec4 color = texture2D(text, uv);\n"
+"    vec4 originalColor = texture2D(text, uv);\n"
+"    float t = time * float(SPEED);\n"
+"    float seed = dot(uv, vec2(12.9898, 78.233));\n"
+"    float noise = fract(sin(seed) * 43758.5453 + t);\n"
+"    noise = gaussian(noise, float(MEAN), float(VARIANCE) * float(VARIANCE));\n"
+"    float w = float(INTENSITY);\n"
+"    vec3 grain = vec3(noise) * (1.0 - color.rgb);\n"
+"    color.rgb += grain * w;\n"
+"    if(isColor){\n"
+"        color.r = color.r * 1.1 + 0.1 ;\n"
+"        color.b = color.b * 0.8 + 0.2 ;\n"
+"        color.g = color.g * color.b * 1.8 - 0.08;\n"
+"    }\n"
+"    gl_FragColor = mix(originalColor,color,power);\n"
+"}\n";
+
+static const GLchar *f_shockwave_shader_source =
+"#define ZERO vec2(0.0,0.0)\n"
+"uniform float power;\n"
+"uniform float u_time;\n"
+
+"varying vec2 texCoord;\n"
+"uniform sampler2D tex;\n"
+"uniform float time;\n"
+"vec3 invert(vec3 rgb)\n"
+"{\n"
+"    return vec3(1.0-rgb.r,1.0-rgb.g,1.0-rgb.b);\n"
+"}\n"
+
+"void handleRadius(out float radius,vec2 uvc,float progress2,float uvcd, out vec3 color, bool isMax){\n"
+"    float pMul = 1.440;\n"
+"    radius *= (cos(uvc.x * 33.752 + pMul * progress2) * 0.024 + (1.0 - uvcd*0.040)) ;\n"
+"    radius *= (sin(uvc.y * 33.752 + pMul * progress2) * 0.008 + (1.0 - uvcd*0.040)) ;\n"
+
+"    bool inside = uvcd > radius ? true : false;\n"
+"    float ringWidth = inside ? 0.029 : 0.221;\n"
+"    float disFromRing = abs(uvcd - radius);\n"
+"    if(uvcd < radius){\n"
+"    color.rgb = invert(color.rgb);\n"
+"    }\n"
+
+"    if(isMax ? (disFromRing < ringWidth) : (disFromRing > ringWidth)){\n"
+"        float lightPower = inside ? 27.312 : 2.856;\n"
+"        color.rgb += vec3(1.,1.,1.) * lightPower * (ringWidth - disFromRing);\n"
+"    }\n"
+"}\n"
+
+"vec2 SineWave( vec2 p,float tx, float ty )\n"
+"    {\n"
+"    // convert Vertex position <-1,+1> to texture coordinate <0,1> and some shrinking so the effect dont overlap screen\n"
+"    // wave distortion\n"
+"    float x = sin( 25.0*p.y + 30.0*p.x + 6.28*tx) * 0.01;\n"
+"    float y = sin( 25.0*p.y + 30.0*p.x + 6.28*ty) * 0.01;\n"
+"    return vec2(p.x+x, p.y+y);\n"
+"    }\n"
+
+"void main() {\n"
+"    vec2 uv = texCoord;\n"
+"    float progress2 = (power / 2.0 - 0.1) * 1.2;\n"
+"    \n"
+"    vec2 disorted_uv = mix(texCoord, SineWave(texCoord, time, time), power);\n"
+"    \n"
+"    vec3 color = texture2D(tex, disorted_uv).rgb;\n"
+"    vec2 uvc = vec2(0.5,0.5) - uv;\n"
+"    \n"
+"    float minRadius = progress2  - 0.5;\n"
+"    float maxRadius = progress2 * 2.0;\n"
+"    float uvcd = distance(ZERO, uvc);\n"
+"    \n"
+"  	handleRadius(maxRadius,uvc,progress2,uvcd,color,true);\n"
+"    //handleRadius(minRadius,uvc,progress2,uvcd,color,true);\n"
+
+"    gl_FragColor = vec4(color,1.0);\n"
+"}\n";
 
 // thanks to raja https://www.shadertoy.com/view/lsXSDn
 static const GLchar *f_matrix_shader_source =
@@ -288,27 +417,33 @@ static void vbo_setup(GenericShaderContext *gs, AVFilterContext *log_ctx) {
 }
 
 static void tex_setup(AVFilterLink *inlink, AVFilterContext *log_ctx) {
-  AVFilterContext     *ctx = inlink->dst;
-  GenericShaderContext *gs = ctx->priv;
-  av_log(log_ctx, AV_LOG_VERBOSE, "tex_setup\n");
+    AVFilterContext     *ctx = inlink->dst;
+    GenericShaderContext *gs = ctx->priv;
+    av_log(log_ctx, AV_LOG_VERBOSE, "tex_setup\n");
 
-  glGenTextures(1, &gs->frame_tex);
-  glActiveTexture(GL_TEXTURE0);
+    glGenTextures(1, &gs->frame_tex);
+    glActiveTexture(GL_TEXTURE0);
 
-  glBindTexture(GL_TEXTURE_2D, gs->frame_tex);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, gs->frame_tex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, inlink->w, inlink->h, 0, PIXEL_FORMAT, GL_UNSIGNED_BYTE, NULL);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, inlink->w, inlink->h, 0, PIXEL_FORMAT, GL_UNSIGNED_BYTE, NULL);
 
-  glUniform1i(glGetUniformLocation(gs->program, "tex"), 0);
-  if (!strcmp(gs->shader_style, "matrix")){
-    glUniform1f(glGetUniformLocation(gs->program, "power"), 0);
-    glUniform1f(glGetUniformLocation(gs->program, "time"), 0);
-  }
-  av_log(log_ctx, AV_LOG_VERBOSE, "tex_setup end\n");
+    glUniform1i(glGetUniformLocation(gs->program, "tex"), 0);
+    if (!strcmp(gs->shader_style, "matrix")){
+        glUniform1f(glGetUniformLocation(gs->program, "power"), 0);
+        glUniform1f(glGetUniformLocation(gs->program, "time"), 0);
+    } else if (!strcmp(gs->shader_style, "shockwave")){
+        glUniform1f(glGetUniformLocation(gs->program, "power"), 0);
+        glUniform1f(glGetUniformLocation(gs->program, "time"), 0);
+    } else if (!strcmp(gs->shader_style, "vintage")){
+        glUniform1f(glGetUniformLocation(gs->program, "power"), 0);
+        glUniform1f(glGetUniformLocation(gs->program, "time"), 0);
+    }
+    av_log(log_ctx, AV_LOG_VERBOSE, "tex_setup end\n");
 }
 
 static int build_program(AVFilterContext *ctx) {
@@ -324,10 +459,17 @@ static int build_program(AVFilterContext *ctx) {
     if (!strcmp(gs->shader_style, "matrix")){
         av_log(ctx, AV_LOG_VERBOSE, "build_program_2_1 %d\n", v_shader);
         f_shader = build_shader(ctx, f_matrix_shader_source, GL_FRAGMENT_SHADER);
-    } else {
+    } else if (!strcmp(gs->shader_style, "shockwave")) {
         av_log(ctx, AV_LOG_VERBOSE, "build_program_2_2 %d\n", v_shader);
+        f_shader = build_shader(ctx, f_shockwave_shader_source, GL_FRAGMENT_SHADER);
+    } else if (!strcmp(gs->shader_style, "vintage")) {
+        av_log(ctx, AV_LOG_VERBOSE, "build_program_2_3 %d\n", v_shader);
+        f_shader = build_shader(ctx, f_vintage_shader_source, GL_FRAGMENT_SHADER);
+    } else {
+        av_log(ctx, AV_LOG_VERBOSE, "build_program_2_0 %d\n", v_shader);
         f_shader = build_shader(ctx, f_shader_source, GL_FRAGMENT_SHADER);
     }
+
     av_log(ctx, AV_LOG_VERBOSE, "build_program_3 %d\n", f_shader);
 
     if (!(v_shader && f_shader)) {
@@ -441,6 +583,18 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in) {
         GLfloat time = (GLfloat)(gs->var_values[VAR_T] == NAN? gs->frame_idx * 330: gs->var_values[VAR_T] * 1000);
         av_log(ctx, AV_LOG_VERBOSE, "filter_frame matrix time:%f\n", time);
         glUniform1fv(glGetUniformLocation(gs->program, "time"), 1, &time);
+    } else if (!strcmp(gs->shader_style, "shockwave")){
+        glUniform1fv(glGetUniformLocation(gs->program, "power"), 1, &gs->power);
+        GLfloat time = (GLfloat)(gs->var_values[VAR_T] == NAN? gs->frame_idx * 1.6667: gs->var_values[VAR_T] * 5);
+        av_log(ctx, AV_LOG_VERBOSE, "filter_frame shockwave time:%f\n", time);
+        glUniform1fv(glGetUniformLocation(gs->program, "time"), 1, &time);
+    } else if (!strcmp(gs->shader_style, "vintage")){
+        glUniform1fv(glGetUniformLocation(gs->program, "power"), 1, &gs->power);
+        GLfloat time = (GLfloat)(gs->var_values[VAR_T] == NAN? gs->frame_idx * 330: gs->var_values[VAR_T] * 1000);
+        GLint isColor = gs->is_color == IS_COLOR_MODE_TRUE? 1: 0;
+        av_log(ctx, AV_LOG_VERBOSE, "filter_frame vintage isColor:%d time:%f\n", isColor, time);
+        glUniform1fv(glGetUniformLocation(gs->program, "time"), 1, &time);
+        glUniform1iv(glGetUniformLocation(gs->program, "isColor"), 1, &isColor);
     }
     glDrawArrays(GL_TRIANGLES, 0, 6);
     glReadPixels(0, 0, outlink->w, outlink->h, PIXEL_FORMAT, GL_UNSIGNED_BYTE, (GLvoid *)out->data[0]);
@@ -472,6 +626,9 @@ static const AVOption genericshader_options[] = {
     { "eval", "specify when to evaluate expressions", OFFSET(eval_mode), AV_OPT_TYPE_INT, {.i64 = EVAL_MODE_FRAME}, 0, EVAL_MODE_NB-1, FLAGS, "eval" },
              { "init",  "eval expressions once during initialization", 0, AV_OPT_TYPE_CONST, {.i64=EVAL_MODE_INIT},  .flags = FLAGS, .unit = "eval" },
              { "frame", "eval expressions per-frame",                  0, AV_OPT_TYPE_CONST, {.i64=EVAL_MODE_FRAME}, .flags = FLAGS, .unit = "eval" },
+    { "is_color", "relevant to vintage, specify color mode", OFFSET(is_color), AV_OPT_TYPE_INT, {.i64 = IS_COLOR_MODE_TRUE}, 0, IS_COLOR_MODE_NB-1, FLAGS, "eval" },
+             { "true",  "color mode",    0, AV_OPT_TYPE_CONST, {.i64=IS_COLOR_MODE_TRUE},  .flags = FLAGS, .unit = "eval" },
+             { "false", "no color mode", 0, AV_OPT_TYPE_CONST, {.i64=IS_COLOR_MODE_FALSE}, .flags = FLAGS, .unit = "eval" },
     {NULL}
 };
 
