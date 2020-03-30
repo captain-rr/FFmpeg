03b3303d471f8fa6e53a5861366f252a5704ef33
Dror <dror@rndrobin.com>
2020-03-30 22:03:25 +0300

    Revert "http://git.videolan.org/?p=ffmpeg.git;a=commitdiff;h=cbf2a9bfac60e400980b503061922aedeedd9e28"
    This reverts commit d14d0b6cc5a80ff452987143b7f9e25f9e82dcf6.

--- libavutil/log.c
+++ libavutil/log.c
@@ -55,7 +55,7 @@ static int av_log_level = AV_LOG_INFO;
 static int flags;
 
 #define NB_LEVELS 8
-#if defined(_WIN32) && HAVE_SETCONSOLETEXTATTRIBUTE && HAVE_GETSTDHANDLE
+#if defined(_WIN32) && HAVE_SETCONSOLETEXTATTRIBUTE
 #include <windows.h>
 static const uint8_t color[16 + AV_CLASS_CATEGORY_NB] = {
     [AV_LOG_PANIC  /8] = 12,
@@ -120,34 +120,9 @@ static const uint32_t color[16 + AV_CLASS_CATEGORY_NB] = {
 #endif
 static int use_color = -1;
 
-#if defined(_WIN32) && HAVE_SETCONSOLETEXTATTRIBUTE && HAVE_GETSTDHANDLE
-static void win_console_puts(const char *str)
-{
-    const uint8_t *q = str;
-    uint16_t line[LINE_SZ];
-
-    while (*q) {
-        uint16_t *buf = line;
-        DWORD nb_chars = 0;
-        DWORD written;
-
-        while (*q && nb_chars < LINE_SZ - 1) {
-            uint32_t ch;
-            uint16_t tmp;
-
-            GET_UTF8(ch, *q ? *q++ : 0, ch = 0xfffd; goto continue_on_invalid;)
-continue_on_invalid:
-            PUT_UTF16(ch, tmp, *buf++ = tmp; nb_chars++;)
-        }
-
-        WriteConsoleW(con, line, nb_chars, &written, NULL);
-    }
-}
-#endif
-
 static void check_color_terminal(void)
 {
-#if defined(_WIN32) && HAVE_SETCONSOLETEXTATTRIBUTE && HAVE_GETSTDHANDLE
+#if defined(_WIN32) && HAVE_SETCONSOLETEXTATTRIBUTE
     CONSOLE_SCREEN_BUFFER_INFO con_info;
     con = GetStdHandle(STD_ERROR_HANDLE);
     use_color = (con != INVALID_HANDLE_VALUE) && !getenv("NO_COLOR") &&
@@ -182,10 +157,10 @@ static void colored_fputs(int level, int tint, const char *str)
     if (level == AV_LOG_INFO/8) local_use_color = 0;
     else                        local_use_color = use_color;
 
-#if defined(_WIN32) && HAVE_SETCONSOLETEXTATTRIBUTE && HAVE_GETSTDHANDLE
+#if defined(_WIN32) && HAVE_SETCONSOLETEXTATTRIBUTE
     if (local_use_color)
         SetConsoleTextAttribute(con, background | color[level]);
-    win_console_puts(str);
+    fputs(str, stderr);
     if (local_use_color)
         SetConsoleTextAttribute(con, attr_orig);
 #else
